8dc23b3df3f22c08687fb5d16474bb61cc7283f2
Qendrim Behrami <behrami@in.tum.de>
2024-02-22 23:19:42 +0100

    Implement checking for winner and logging it to console

--- scripts/gameboard.js
+++ scripts/gameboard.js
@@ -2,29 +2,37 @@ class GameBoard {
   cells;
   currentMark;
   availableMarks = ["X", "O"];
+  writeCounter = 0; // To detect draw
+  hoverColor = "#555";
 
   constructor() {
     this.currentMark = 0;
     this.cells = new Array();
     for (let id = 0; id < 9; id++) {
       let newCell = new Cell(id, "").createDiv();
+
+      // Write the current mark if empty and increment to the next mark
       newCell.addEventListener("click", () => {
         // Only write once!
         if (newCell.written) return;
         newCell.style["color"] = "black";
 
         newCell.textContent = this.availableMarks[this.currentMark];
-        this.currentMark = (this.currentMark + 1) % this.availableMarks.length;
         newCell.written = true;
+        this.writeCounter++;
+        this.checkWinner(id);
+        this.currentMark = (this.currentMark + 1) % this.availableMarks.length;
       });
 
+      // Display the current mark if cell is hovered and empty
       newCell.addEventListener("mouseover", () => {
         // Only write once!
         if (newCell.written) return;
         newCell.textContent = this.availableMarks[this.currentMark];
-        newCell.style["color"] = "#777";
+        newCell.style["color"] = this.hoverColor;
       });
 
+      // Reset hover if moved out and no write happened
       newCell.addEventListener("mouseout", () => {
         // Only write once!
         if (newCell.written) return;
@@ -45,6 +53,40 @@ class GameBoard {
       boardElement.appendChild(cell);
     });
     return boardElement;
+  }
+
+  checkWinner(id) {
+    // Left Diagonal
+    if (id == 0 || id == 4 || id == 8) {
+      this.checkHelper(0, 4, 8);
+    }
+
+    // Right Diagonal
+    if (id == 2 || id == 4 || id == 6) {
+      this.checkHelper(2, 4, 6);
+    }
+
+    // Row
+    let row = Math.floor(id / 3) * 3; //Map current ID to first ID of its row
+    this.checkHelper(row, row + 1, row + 2);
+
+    // Column
+    let column = id % 3;
+    this.checkHelper(column, column + 3, column + 6);
+
+    // Check draw
+    if (this.writeCounter == 9) {
+      console.log("Draw");
+    }
+  }
+
+  checkHelper(id1, id2, id3) {
+    if (
+      this.cells[id1].textContent == this.cells[id2].textContent &&
+      this.cells[id2].textContent == this.cells[id3].textContent
+    ) {
+      console.log(`${this.availableMarks[this.currentMark]} wins`);
+    }
   }
 }
 
